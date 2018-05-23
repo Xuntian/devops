fa8211bb8a6208669766ab435e34db2fd99e9dc2
Xuntian <li.zq@foxmail.com>
2018-05-23 03:33:00 -0400

    add git and es

---
+++ gitlab_en/gitlab.rb
@@ -0,0 +1,53 @@
+external_url 'http://git.keepwork.com'
+
+gitlab_rails['db_adapter'] = "postgresql"
+gitlab_rails['db_encoding'] = "unicode"
+gitlab_rails['db_database'] = "gitlab"
+gitlab_rails['db_pool'] = 10
+gitlab_rails['db_username'] = "gitlab"
+gitlab_rails['db_password'] = "123456"
+gitlab_rails['db_host'] = "postgres01.defalut.svc.cluster.local"
+gitlab_rails['db_port'] = 5432
+gitlab_rails['redis_host'] = "redis01.default.svc.cluster.local"
+gitlab_rails['redis_port'] = 6379
+gitlab_rails['redis_database'] = 0
+postgresql['enable'] = false
+redis['enable'] = false
+nginx['enable'] = true
+nginx['redirect_http_to_https'] = true
+
+gitlab_rails['omniauth_enabled'] = true
+gitlab_rails['omniauth_block_auto_created_users'] = true
+gitlab_rails['omniauth_providers'] = [
+   {
+     "name" => "github",
+     "app_id" => "f6ebc98d3308417bde83",
+     "app_secret" => "4a960a11ddf5136fa29a41e9688eba3897094373",
+     "args" => { "scope" => "user:email" }
+   }
+ ]
+
+gitlab_rails['gitlab_ssh_host'] = 'ssh.keepwork.com'
+gitlab_rails['gitlab_shell_ssh_port'] = 22
+gitlab_rails['git_max_size'] = 209715200
+gitlab_rails['smtp_enable'] = true
+gitlab_rails['smtp_address'] = "smtp.exmail.qq.com"
+gitlab_rails['smtp_port'] = 465
+gitlab_rails['smtp_user_name'] = "noreply@mail.keepwork.com"
+gitlab_rails['gitlab_email_from'] = 'noreply@mail.keepwork.com'
+gitlab_rails['smtp_password'] = "M3Hbhq6KAZzagFP4"
+# gitlab_rails['smtp_domain'] = "smtp.qq.com"
+gitlab_rails['smtp_authentication'] = "login"
+gitlab_rails['smtp_enable_starttls_auto'] = true
+gitlab_rails['smtp_tls'] = true
+
+#nginx['client_max_body_size'] = '250m'
+#nginx['client_max_body_size'] = '0'
+
+nginx['custom_gitlab_server_config'] = "location ~ ^/[^a][^p][^i].*\\.*$ {\n proxy_cache gitlab;\n proxy_pass  http://gitlab-workhorse; \n add_header Access-Control-Allow-Origin '*';\n}\n"
+
+git_data_dirs({"default" => "/etc/gitlab/data/git-data"})
+gitlab_rails['uploads_directory'] = '/etc/gitlab/data/uploads'
+gitlab_rails['shared_path'] = '/etc/gitlab/data/shared'
+gitlab_ci['builds_directory'] = '/etc/gitlab/data/builds'
+
